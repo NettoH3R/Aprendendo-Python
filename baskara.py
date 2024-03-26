13542449e647894ad25f7fb963379d5e353f3c4a
João Netto <jovinettonetto@gmail.com>
2024-03-26 01:43:34 +0000

    baskara

---
+++ baskara.py
@@ -0,0 +1,8 @@
+a = float(input('digite o valor de a: '))
+b = float(input('digite o valor de b: '))
+c = float(input('digite o valor de c: '))
+
+delta = b**2 (-4*a*c)
+
+x1 = (-b+delta**(1/2))/(a*2)
+x2 = (-delta**(1/2))/(a*2)
--- ex2.py
+++ ex2.py
@@ -3,3 +3,4 @@
 num = input('Digite um número: ')
 
 print('O número informado foi ' + num)
+
